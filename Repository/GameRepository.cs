1d702dfbbfb8ca4905e20c0d2b21133786f33fb5
Brad Deibert <bradeibert7@gmail.com>
2025-04-12 15:50:29 -0600

    create GameRepository for handling creation of new games in db

---
+++ Repository/GameRepository.cs
@@ -0,0 +1,57 @@
+using game_of_life.Models.Service;
+using game_of_life.Models.Data;
+
+namespace game_of_life.Repository;
+
+public interface IGameRepository
+{
+  Task<Game> CreateGameAsync(IList<Models.Service.GameCell> cells);
+}
+
+public class GameRepository : IGameRepository
+{
+  private readonly GameDbContext _context;
+
+  public GameRepository(GameDbContext context)
+  {
+    _context = context;
+  }
+
+  public async Task<Game> CreateGameAsync(IList<Models.Service.GameCell> cells)
+  {
+    if (cells == null || cells.Count == 0)
+    {
+      throw new ArgumentException("Game must have at least one cell.");
+    }
+
+    // Create a new game instance
+    var game = new Game
+    {
+      GameId = 0, // This will be set by the database
+      CreatedAt = DateTime.UtcNow,
+      UpdatedAt = DateTime.UtcNow,
+    };
+
+    game.Cells = cells.Select(cell => new Models.Data.GameCell
+    {
+      GameCellId = 0, // This will be set by the database
+      GameId = game.GameId, // This will be set later
+      X = cell.X,
+      Y = cell.Y,
+    }).ToList();
+
+    // Add the game 
+    _context.Games.Add(game);
+
+    // Add each cell for game
+    foreach (var cell in game.Cells)
+    {
+      cell.GameId = game.GameId; // link to game record
+      _context.GameCells.Add(cell);
+    }
+
+    await _context.SaveChangesAsync();
+
+    return game;
+  }
+}
